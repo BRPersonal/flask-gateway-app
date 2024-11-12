49a58909ddbe1ec7dff138a14514ab4233c157bf
BRPersonal <balajirengan@gmail.com>
2024-11-12 18:41:16 +0530

    Coding and testing completed. One issue remains

--- controller.py
+++ controller.py
@@ -4,10 +4,6 @@ from api_response import ApiResponse
 
 app = Flask(__name__)
 
-@app.route('/get-key/<key>', methods=['GET'])
-def get_key(key) -> (Response,str):
-    result = get_key_details(key)
-    return jsonify(result), result['status_code']
 
 @app.route('/create-key', methods=['POST'])
 def key_create() -> (Response,str):
@@ -24,6 +20,35 @@ def key_create() -> (Response,str):
 
     return jsonify(result), result['status_code']
 
+@app.route('/list-keys', methods=['GET'])
+def listKeys() -> (Response,str):
+    result = list_keys()
+    return jsonify(result), result['status_code']
+
+@app.route('/get-key/<key>', methods=['GET'])
+def get_key(key:str) -> (Response,str):
+    result = get_key_details(key)
+    return jsonify(result), result['status_code']
+
+@app.route('/update-plan', methods=['PUT'])
+def updateKeyPlan() -> (Response,str):
+    api_response: ApiResponse = None
+    result : dict = None
+
+    post_data = request.get_json()
+    if not post_data or "plan" not in post_data or "key" not in post_data:
+        api_response = ApiResponse(error="Missing 'plan' or 'key' in request body",
+                                   statuscode=400)
+        result = api_response.to_json()
+    else:
+        result = update_key_plan(post_data)
+
+    return jsonify(result), result['status_code']
+
+@app.route('/delete-key/<key>', methods=['DELETE'])
+def keyDeletion(key: str) -> (Response,str):
+    result = delete_key(key)
+    return jsonify(result), result['status_code']
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
--- service.py
+++ service.py
@@ -16,35 +16,6 @@ print(f"authorization={TYK_AUTHORIZATION}")
 print(f"baseUrl={TYK_BASE_URL}")
 print(f"OrgId={ORG_ID}")
 
-def get_key_details(key: str) -> dict:
-    api_response : ApiResponse = None
-
-    try:
-        tyk_response = requests.get(
-            f"{TYK_BASE_URL}/tyk/keys/{key}",
-            headers={
-                "Content-Type": "application/json",
-                "x-tyk-authorization": TYK_AUTHORIZATION
-            }
-        )
-
-        if tyk_response.status_code == 200 or tyk_response.status_code == 201:
-            api_response = ApiResponse(message="Key details retrieved successfully",
-                                       statuscode=tyk_response.status_code,
-                                       response=tyk_response.json())
-        else:
-            api_response = ApiResponse(error="Key details retrieval Failed",
-                                       statuscode=tyk_response.status_code,
-                                       response=tyk_response.text)
-
-    except Exception as e:
-        api_response = ApiResponse(error="Request failed",
-                                   statuscode=INTERNAL_SERVER_ERROR,
-                                   response=str(e)
-                                   )
-
-    return api_response.to_json()
-
 def create_key(plan: str) -> dict:
     api_response: ApiResponse = None
     post_data = {
@@ -61,8 +32,6 @@ def create_key(plan: str) -> dict:
         "throttle_interval": 10,
         "throttle_retry_limit": 10
     }
-
-    print(json.dumps(post_data))
 
     try:
         tyk_response = requests.post(
@@ -88,4 +57,126 @@ def create_key(plan: str) -> dict:
                                    statuscode=INTERNAL_SERVER_ERROR,
                                    response=str(e)
                                    )
+    return api_response.to_json()
+
+def list_keys() -> dict:
+    api_response : ApiResponse = None
+
+    try:
+        tyk_response = requests.get(
+            f"{TYK_BASE_URL}/tyk/keys",
+            headers={
+                "Content-Type": "application/json",
+                "x-tyk-authorization": TYK_AUTHORIZATION
+            }
+        )
+
+        if tyk_response.status_code == 200 or tyk_response.status_code == 201:
+            api_response = ApiResponse(message="Keys retrieved successfully",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.json())
+        else:
+            api_response = ApiResponse(error="Keys retrieval Failed",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.text)
+
+    except Exception as e:
+        api_response = ApiResponse(error="Request failed",
+                                   statuscode=INTERNAL_SERVER_ERROR,
+                                   response=str(e)
+                                   )
+
+    return api_response.to_json()
+
+def get_key_details(key: str) -> dict:
+    api_response : ApiResponse = None
+
+    try:
+        tyk_response = requests.get(
+            f"{TYK_BASE_URL}/tyk/keys/{key}",
+            headers={
+                "Content-Type": "application/json",
+                "x-tyk-authorization": TYK_AUTHORIZATION
+            }
+        )
+
+        if tyk_response.status_code == 200 or tyk_response.status_code == 201:
+            api_response = ApiResponse(message=f"Key {key} retrieved successfully",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.json())
+        else:
+            api_response = ApiResponse(error=f"Key {key} retrieval Failed",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.text)
+
+    except Exception as e:
+        api_response = ApiResponse(error=f"Request failed for Key {key}",
+                                   statuscode=INTERNAL_SERVER_ERROR,
+                                   response=str(e)
+                                   )
+
+    return api_response.to_json()
+
+def update_key_plan(request_body:dict) -> dict:
+    api_response: ApiResponse = None
+
+    plan = request_body["plan"]
+    key = request_body["key"]
+    post_data = {
+        "apply_policies": [plan]
+    }
+
+    try:
+        tyk_response = requests.post(
+            f"{TYK_BASE_URL}/tyk/keys/{key}",
+            headers={
+                "Content-Type": "application/json",
+                "x-tyk-authorization": TYK_AUTHORIZATION
+            },
+            data=json.dumps(post_data)
+        )
+
+        if tyk_response.status_code == 200 or tyk_response.status_code == 201:
+            api_response = ApiResponse(message=f"Plan updation for key {key} successful",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.json())
+        else:
+            api_response = ApiResponse(error=f"Plan updation for key {key} Failed",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.text)
+
+    except Exception as e:
+        api_response = ApiResponse(error="Request failed",
+                                   statuscode=INTERNAL_SERVER_ERROR,
+                                   response=str(e)
+                                   )
+    return api_response.to_json()
+
+def delete_key(key:str) -> dict:
+    api_response : ApiResponse = None
+
+    try:
+        tyk_response = requests.delete(
+            f"{TYK_BASE_URL}/tyk/keys/{key}",
+            headers={
+                "Content-Type": "application/json",
+                "x-tyk-authorization": TYK_AUTHORIZATION
+            }
+        )
+
+        if tyk_response.status_code == 200 or tyk_response.status_code == 201:
+            api_response = ApiResponse(message=f"Key {key} deleted successfully",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.json())
+        else:
+            api_response = ApiResponse(error=f"Key {key} deletion failed",
+                                       statuscode=tyk_response.status_code,
+                                       response=tyk_response.text)
+
+    except Exception as e:
+        api_response = ApiResponse(error="Request failed",
+                                   statuscode=INTERNAL_SERVER_ERROR,
+                                   response=str(e)
+                                   )
+
     return api_response.to_json()
